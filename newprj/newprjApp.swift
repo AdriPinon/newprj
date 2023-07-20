a5035a751e521cd8c8460175608fa172ac4bfadc
Adriana Pinon <adrianapinon000@gmail.com>
2023-07-20 12:24:43 -0700

    Initial Commit

---
+++ newprj/newprjApp.swift
@@ -0,0 +1,17 @@
+//
+//  newprjApp.swift
+//  newprj
+//
+//  Created by Adriana Pinon on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct newprjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
